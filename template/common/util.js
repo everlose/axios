e675af39a80bafafe444274ce0d8d4c3c0145410
陈子云 <472206618@qq.com>
2018-03-06 17:03:51 +0800

    Update util.js

--- template/common/util.js
+++ template/common/util.js
@@ -5,11 +5,22 @@ const instance = axios.create();
 function createAPI(baseURL) {
   return function (conf) {
     conf = conf || {};
-    return instance(Object.assign({}, {
+    let axiosInstance = instance(Object.assign({}, {
       url: conf.url,
       baseURL: baseURL,
       method: conf.method
     }, conf.opts));
+    return new Promise((resolve, reject) => {
+      axiosInstance.then((d) => {
+        if (d.data.success === true) {
+          resolve(d.data.data, d);
+        } else {
+          reject(d.data.msg);
+        }
+      }, (e) => {
+        reject(e);
+      });
+    });
   };
 }
 
